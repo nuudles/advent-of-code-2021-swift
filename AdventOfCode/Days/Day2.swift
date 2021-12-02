f4d6a309e962f8346f2348a1a2b445de46063a81
Christopher Luu <nuudles@gmail.com>
2021-12-02 10:43:11 -0500

    Add Day 2 solution

--- AdventOfCode/Days/Day2.swift
+++ AdventOfCode/Days/Day2.swift
@@ -7,10 +7,46 @@ import Foundation
 
 final class Day2: Day {
     func part1(_ input: String) -> CustomStringConvertible {
-        return 0
+        var x = 0
+        var y = 0
+        input
+            .split(separator: "\n")
+            .forEach { line in
+                let command = line.components(separatedBy: " ")
+                switch command[0] {
+                case "forward":
+                    x += Int(command[1]) ?? 0
+                case "down":
+                    y += Int(command[1]) ?? 0
+                case "up":
+                    y -= Int(command[1]) ?? 0
+                default:
+                    break
+                }
+            }
+        return x * y
     }
 
     func part2(_ input: String) -> CustomStringConvertible {
-        return 0
+        var x = 0
+        var y = 0
+        var aim = 0
+        input
+            .split(separator: "\n")
+            .forEach { line in
+                let command = line.components(separatedBy: " ")
+                switch command[0] {
+                case "forward":
+                    x += Int(command[1]) ?? 0
+                    y += (Int(command[1]) ?? 0) * aim
+                case "down":
+                    aim += Int(command[1]) ?? 0
+                case "up":
+                    aim -= Int(command[1]) ?? 0
+                default:
+                    break
+                }
+            }
+        return x * y
     }
 }
