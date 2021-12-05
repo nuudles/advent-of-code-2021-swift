6b40e063d39689fd60a9479f4a35f64cbcd67f98
Christopher Luu <nuudles@gmail.com>
2021-12-05 08:33:50 -0500

    Add Day 5 solution

--- AdventOfCode/Days/Day5.swift
+++ AdventOfCode/Days/Day5.swift
@@ -7,10 +7,63 @@ import Foundation
 
 final class Day5: Day {
     func part1(_ input: String) -> CustomStringConvertible {
-        return 0
+        var intersections = [String: Int]()
+        input
+            .lines
+            .forEach { line in
+                let components = line
+                    .components(separatedBy: " -> ")
+                    .map { component -> (Int, Int) in
+                        let nums = component
+                            .split(separator: ",")
+                            .compactMap { Int($0) }
+                        return (nums.first!, nums.last!)
+                    }
+                let (start, end) = (components[0], components[1])
+                if start.0 == end.0 {
+                    for y in min(start.1, end.1)...max(start.1, end.1) {
+                        let key = "\(start.0),\(y)"
+                        intersections[key] = (intersections[key] ?? 0) + 1
+                    }
+                } else if start.1 == end.1 {
+                    for x in min(start.0, end.0)...max(start.0, end.0) {
+                        let key = "\(x),\(start.1)"
+                        intersections[key] = (intersections[key] ?? 0) + 1
+                    }
+                }
+            }
+        return intersections.count { $0.value > 1 }
     }
 
     func part2(_ input: String) -> CustomStringConvertible {
-        return 0
+        var intersections = [String: Int]()
+        input
+            .lines
+            .forEach { line in
+                let components = line
+                    .components(separatedBy: " -> ")
+                    .map { component -> (Int, Int) in
+                        let nums = component
+                            .split(separator: ",")
+                            .compactMap { Int($0) }
+                        return (nums.first!, nums.last!)
+                    }
+                let (start, end) = (components[0], components[1])
+                let (dx, dy) = (
+                    start.0 == end.0 ? 0 : start.0 < end.0 ? 1 : -1,
+                    start.1 == end.1 ? 0 : start.1 < end.1 ? 1 : -1
+                )
+                var point = start
+                while point != end {
+                    let key = "\(point.0),\(point.1)"
+                    intersections[key] = (intersections[key] ?? 0) + 1
+                    point.0 += dx
+                    point.1 += dy
+                }
+
+                let key = "\(point.0),\(point.1)"
+                intersections[key] = (intersections[key] ?? 0) + 1
+            }
+        return intersections.count { $0.value > 1 }
     }
 }
--- AdventOfCodeTests/Day5Tests.swift
+++ AdventOfCodeTests/Day5Tests.swift
@@ -9,11 +9,39 @@ class Day5Tests: XCTestCase {
     let day = Day5()
 
     func testPart1() throws {
-        debugPrint(day.part1(""))
+        XCTAssertEqual(
+            day.part1("""
+            0,9 -> 5,9
+            8,0 -> 0,8
+            9,4 -> 3,4
+            2,2 -> 2,1
+            7,0 -> 7,4
+            6,4 -> 2,0
+            0,9 -> 2,9
+            3,4 -> 1,4
+            0,0 -> 8,8
+            5,5 -> 8,2
+            """) as? Int,
+            5
+        )
     }
 
     func testPart2() throws {
-        debugPrint(day.part2(""))
+        XCTAssertEqual(
+            day.part2("""
+            0,9 -> 5,9
+            8,0 -> 0,8
+            9,4 -> 3,4
+            2,2 -> 2,1
+            7,0 -> 7,4
+            6,4 -> 2,0
+            0,9 -> 2,9
+            3,4 -> 1,4
+            0,0 -> 8,8
+            5,5 -> 8,2
+            """) as? Int,
+            12
+        )
     }
 
 }
