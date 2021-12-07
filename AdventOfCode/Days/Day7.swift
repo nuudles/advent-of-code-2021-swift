cf722596450edc1ec9c410504d5ad09fdb00f934
Christopher Luu <nuudles@gmail.com>
2021-12-07 12:42:47 -0500

    Add Day 7 solution

--- AdventOfCode/Days/Day7.swift
+++ AdventOfCode/Days/Day7.swift
@@ -7,10 +7,25 @@ import Foundation
 
 final class Day7: Day {
     func part1(_ input: String) -> CustomStringConvertible {
-        return 0
+        let crabs = input.lines.first!.split(separator: ",").compactMap { Int($0) }
+        let (min, max) = (crabs.min() ?? 0, crabs.max() ?? 0)
+        return (min...max)
+            .map { i in crabs.reduce(0, { $0 + abs($1 - i) }) }
+            .min()!
     }
 
     func part2(_ input: String) -> CustomStringConvertible {
-        return 0
+        let crabs = input.lines.first!.split(separator: ",").compactMap { Int($0) }
+        let (min, max) = (crabs.min() ?? 0, crabs.max() ?? 0)
+        var sum = 0
+        var counts = Array<Int>()
+
+        for i in 0...max {
+            counts.append(sum + i)
+            sum += i
+        }
+        return (min...max)
+            .map { i in crabs.reduce(0, { $0 + counts[abs($1 - i)] }) }
+            .min()!
     }
 }
--- AdventOfCodeTests/Day7Tests.swift
+++ AdventOfCodeTests/Day7Tests.swift
@@ -9,11 +9,11 @@ class Day7Tests: XCTestCase {
     let day = Day7()
 
     func testPart1() throws {
-        debugPrint(day.part1(""))
+        XCTAssertEqual(day.part1("16,1,2,0,4,2,7,1,2,14") as? Int, 37)
     }
 
     func testPart2() throws {
-        debugPrint(day.part2(""))
+        XCTAssertEqual(day.part2("16,1,2,0,4,2,7,1,2,14") as? Int, 168)
     }
 
 }
