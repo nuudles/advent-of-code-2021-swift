b856fba30d47d4a46bf5745ba941d653261202d5
Christopher Luu <nuudles@gmail.com>
2021-12-11 08:44:18 -0500

    Add Day 11 solution

--- AdventOfCode/Days/Day11.swift
+++ AdventOfCode/Days/Day11.swift
@@ -6,11 +6,64 @@
 import Foundation
 
 final class Day11: Day {
+    struct Position: Hashable, Equatable {
+        let x: Int
+        let y: Int
+    }
+
+    func step(_ octopi: [[Int]]) -> (Int, [[Int]]) {
+        var flashPositions = Set<Position>()
+        var newOctopi = octopi.enumerated().map { line in
+            line.element.enumerated().map { (index, energy) -> Int in
+                if energy >= 9 {
+                    flashPositions.insert(Position(x: index, y: line.offset))
+                }
+                return energy + 1
+            }
+        }
+        var newFlashes = flashPositions
+        while newFlashes.count > 0 {
+            var nextFlashes = Set<Position>()
+            newFlashes.forEach { flash in
+                for j in max(0, flash.y - 1)...min(newOctopi.count - 1, flash.y + 1) {
+                    for i in max(0, flash.x - 1)...min(newOctopi[j].count - 1, flash.x + 1) {
+                        let position = Position(x: i, y: j)
+                        guard position != flash else { continue }
+                        newOctopi[j][i] += 1
+                        if newOctopi[j][i] > 9 && !flashPositions.contains(position) {
+                            flashPositions.insert(position)
+                            nextFlashes.insert(position)
+                        }
+                    }
+                }
+            }
+            newFlashes = nextFlashes
+        }
+        flashPositions.forEach {
+            newOctopi[$0.y][$0.x] = 0
+        }
+        return (flashPositions.count, newOctopi)
+    }
+
     func part1(_ input: String) -> CustomStringConvertible {
-        return 0
+        var octopi = input.lines.map { line in line.compactMap { $0.wholeNumberValue } }
+        var flashes = 0
+        for _ in 0..<100 {
+            let (newFlashes, newOctopi) = step(octopi)
+            flashes += newFlashes
+            octopi = newOctopi
+        }
+        return flashes
     }
 
     func part2(_ input: String) -> CustomStringConvertible {
-        return 0
+        var octopi = input.lines.map { line in line.compactMap { $0.wholeNumberValue } }
+        var steps = 0
+        while octopi.map({ $0.sum }).sum != 0 {
+            let (_, newOctopi) = step(octopi)
+            steps += 1
+            octopi = newOctopi
+        }
+        return steps
     }
 }
--- AdventOfCodeTests/Day11Tests.swift
+++ AdventOfCodeTests/Day11Tests.swift
@@ -9,11 +9,46 @@ class Day11Tests: XCTestCase {
     let day = Day11()
 
     func testPart1() throws {
-        debugPrint(day.part1(""))
+//        day.part1("""
+//            11111
+//            19991
+//            19191
+//            19991
+//            11111
+//            """)
+        XCTAssertEqual(
+            day.part1("""
+            5483143223
+            2745854711
+            5264556173
+            6141336146
+            6357385478
+            4167524645
+            2176841721
+            6882881134
+            4846848554
+            5283751526
+            """) as? Int,
+            1656
+        )
     }
 
     func testPart2() throws {
-        debugPrint(day.part2(""))
+        XCTAssertEqual(
+            day.part2("""
+            5483143223
+            2745854711
+            5264556173
+            6141336146
+            6357385478
+            4167524645
+            2176841721
+            6882881134
+            4846848554
+            5283751526
+            """) as? Int,
+            195
+        )
     }
 
 }
