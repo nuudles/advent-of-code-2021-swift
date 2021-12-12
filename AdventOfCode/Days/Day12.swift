97139609c2c37e1ee60927ff44e3c29720c5774f
Christopher Luu <nuudles@gmail.com>
2021-12-12 08:46:43 -0500

    Add Day 12 solution

--- AdventOfCode/Days/Day12.swift
+++ AdventOfCode/Days/Day12.swift
@@ -6,11 +6,92 @@
 import Foundation
 
 final class Day12: Day {
+    func pathsToEnd(
+        from start: String,
+        seen: Set<String>,
+        connections: Dictionary<String, Set<String>>,
+        isPartTwo: Bool,
+        canSeeTwice: String?,
+        hasSeenTwice: Bool
+    ) -> Set<Array<String>> {
+        let canOnlyVisitOnce = start == start.lowercased()
+
+        if start == "end" {
+            return Set([["end"]])
+        } else if canOnlyVisitOnce &&
+                    seen.contains(start) &&
+                    (canSeeTwice != start || hasSeenTwice) {
+            return Set()
+        }
+
+        return connections[start, default: Set()]
+            .filter { $0 != "start" }
+            .reduce(into: Set<Array<String>>()) { result, connection in
+                pathsToEnd(
+                    from: connection,
+                    seen: seen.union([start]),
+                    connections: connections,
+                    isPartTwo: isPartTwo,
+                    canSeeTwice: canSeeTwice,
+                    hasSeenTwice: hasSeenTwice || canSeeTwice == start
+                ).forEach { path in
+                    var newPath = path
+                    newPath.append(start)
+                    result.insert(newPath)
+                }
+
+                if isPartTwo && canOnlyVisitOnce && canSeeTwice == nil {
+                    pathsToEnd(
+                        from: connection,
+                        seen: seen.union([start]),
+                        connections: connections,
+                        isPartTwo: isPartTwo,
+                        canSeeTwice: start,
+                        hasSeenTwice: false
+                    ).forEach { path in
+                        var newPath = path
+                        newPath.append(start)
+                        result.insert(newPath)
+                    }
+                }
+            }
+    }
+
     func part1(_ input: String) -> CustomStringConvertible {
-        return 0
+        let connections = input
+            .lines
+            .reduce(into: Dictionary<String, Set<String>>()) { dictionary, line in
+                let components = line.components(separatedBy: "-")
+                let (left, right) = (components.first!, components.last!)
+                dictionary[left] = dictionary[left, default: Set()].union([right])
+                dictionary[right] = dictionary[right, default: Set()].union([left])
+            }
+        return pathsToEnd(
+            from: "start",
+            seen: Set(),
+            connections: connections,
+            isPartTwo: false,
+            canSeeTwice: nil,
+            hasSeenTwice: false
+        ).count
     }
 
     func part2(_ input: String) -> CustomStringConvertible {
-        return 0
+        let connections = input
+            .lines
+            .reduce(into: Dictionary<String, Set<String>>()) { dictionary, line in
+                let components = line.components(separatedBy: "-")
+                let (left, right) = (components.first!, components.last!)
+                dictionary[left] = dictionary[left, default: Set()].union([right])
+                dictionary[right] = dictionary[right, default: Set()].union([left])
+            }
+        return pathsToEnd(
+            from: "start",
+            seen: Set(),
+            connections: connections,
+            isPartTwo: true,
+            canSeeTwice: nil,
+            hasSeenTwice: false
+        ).count
     }
 }
--- AdventOfCodeTests/Day12Tests.swift
+++ AdventOfCodeTests/Day12Tests.swift
@@ -9,11 +9,109 @@ class Day12Tests: XCTestCase {
     let day = Day12()
 
     func testPart1() throws {
-        debugPrint(day.part1(""))
+        XCTAssertEqual(
+            day.part1("""
+            start-A
+            start-b
+            A-c
+            A-b
+            b-d
+            A-end
+            b-end
+            """) as? Int,
+            10
+        )
+        XCTAssertEqual(
+            day.part1("""
+            dc-end
+            HN-start
+            start-kj
+            dc-start
+            dc-HN
+            LN-dc
+            HN-end
+            kj-sa
+            kj-HN
+            kj-dc
+            """) as? Int,
+            19
+        )
+        XCTAssertEqual(
+            day.part1("""
+            fs-end
+            he-DX
+            fs-he
+            start-DX
+            pj-DX
+            end-zg
+            zg-sl
+            zg-pj
+            pj-he
+            RW-he
+            fs-DX
+            pj-RW
+            zg-RW
+            start-pj
+            he-WI
+            zg-he
+            pj-fs
+            start-RW
+            """) as? Int,
+            226
+        )
     }
 
     func testPart2() throws {
-        debugPrint(day.part2(""))
+        XCTAssertEqual(
+            day.part2("""
+            start-A
+            start-b
+            A-c
+            A-b
+            b-d
+            A-end
+            b-end
+            """) as? Int,
+            36
+        )
+        XCTAssertEqual(
+            day.part2("""
+            dc-end
+            HN-start
+            start-kj
+            dc-start
+            dc-HN
+            LN-dc
+            HN-end
+            kj-sa
+            kj-HN
+            kj-dc
+            """) as? Int,
+            103
+        )
+        XCTAssertEqual(
+            day.part2("""
+            fs-end
+            he-DX
+            fs-he
+            start-DX
+            pj-DX
+            end-zg
+            zg-sl
+            zg-pj
+            pj-he
+            RW-he
+            fs-DX
+            pj-RW
+            zg-RW
+            start-pj
+            he-WI
+            zg-he
+            pj-fs
+            start-RW
+            """) as? Int,
+            3509
+        )
     }
 
 }
